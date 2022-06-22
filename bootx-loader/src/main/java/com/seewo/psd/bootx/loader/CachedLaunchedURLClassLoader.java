d7b5b2d419411d9a343078697aeaeca7c6e5c4d8
Arthur.Zhang <zhangya_no1@qq.com>
2022-06-22 11:22:27 +0800

    add

--- bootx-loader/src/main/java/com/seewo/psd/bootx/loader/CachedLaunchedURLClassLoader.java
+++ bootx-loader/src/main/java/com/seewo/psd/bootx/loader/CachedLaunchedURLClassLoader.java
@@ -1,106 +1,107 @@
 package com.seewo.psd.bootx.loader;
 
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.boot.loader.LaunchedURLClassLoader;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.Enumeration;
+import java.util.Map;
 import java.util.Optional;
-
-import static java.util.concurrent.TimeUnit.SECONDS;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class CachedLaunchedURLClassLoader extends LaunchedURLClassLoader {
-    static {
-        ClassLoader.registerAsParallelCapable();
-    }
+	static {
+		ClassLoader.registerAsParallelCapable();
+	}
 
-    protected Cache<String, LoadClassResult> classCache = Caffeine.newBuilder().initialCapacity(8000)
-            .maximumSize(8000)
-            .expireAfterWrite(120, SECONDS).recordStats().build();
-    protected Cache<String, Optional<URL>> resourceUrlCache = Caffeine.newBuilder().maximumSize(4000)
-            .expireAfterWrite(60, SECONDS).build();
-    protected Cache<String, Optional<Enumeration<URL>>> resourcesUrlCache = Caffeine.newBuilder().maximumSize(4000)
-            .expireAfterWrite(60, SECONDS).build();
+	private final Map<String, LoadClassResult> classCache = new ConcurrentHashMap<>(3000);
+	private final Map<String, Optional<URL>>              resourceUrlCache  = new ConcurrentHashMap<>(
+			3000);
+	private final Map<String, Optional<Enumeration<URL>>> resourcesUrlCache = new ConcurrentHashMap<>(
+			300);
+	public CachedLaunchedURLClassLoader(URL[] urls, ClassLoader parent) {
+		super(urls, parent);
+	}
 
-    public CachedLaunchedURLClassLoader(URL[] urls, ClassLoader parent) {
-        super(urls, parent);
-        System.out.println(">>>>>>>>in CachedLaunchedURLClassLoader");
-    }
+	@Override
+	protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+		return loadClassWithCache(name, resolve);
+	}
 
-    @Override
-    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
-        return loadClassWithCache(name, resolve);
-    }
+	@Override
+	public URL findResource(String name) {
+		Optional<URL> urlOptional = resourceUrlCache.get(name);
+		if (urlOptional != null) {
+			return urlOptional.orElse(null);
+		}
+		URL url = super.findResource(name);
+		resourceUrlCache.put(name, url != null ? Optional.of(url) : Optional.empty());
+		return url;
+	}
 
-    @Override
-    public URL findResource(String name) {
-        Optional<URL> urlOptional = resourceUrlCache.get(name, it -> {
-            URL url = CachedLaunchedURLClassLoader.super.findResource(name);
-            return url != null ? Optional.of(url) : Optional.empty();
-        });
-        return urlOptional.orElse(null);
-    }
+	@Override
+	public Enumeration<URL> findResources(String name) throws IOException {
+		Optional<Enumeration<URL>> urlOptional = resourcesUrlCache.get(name);
+		if (urlOptional != null) {
+			return urlOptional.orElse(null);
+		}
+		Enumeration<URL> enumeration = super.findResources(name);
+		if (enumeration == null || !enumeration.hasMoreElements()) {
+			resourcesUrlCache.put(name, Optional.empty());
+		}
+		return enumeration;
+	}
 
-    @Override
-    public Enumeration<URL> findResources(String name) throws IOException {
-        Optional<Enumeration<URL>> urlOptional = resourcesUrlCache.get(name, it -> {
-                    Enumeration<URL> enumeration = null;
-                    try {
-                        enumeration = CachedLaunchedURLClassLoader.super.findResources(name);
-                    } catch (IOException e) {
-                    }
-                    return (enumeration != null && enumeration.hasMoreElements()) ? Optional.of(enumeration) : Optional.empty();
-                }
-        );
-        return urlOptional.orElse(null);
-    }
+	protected static class LoadClassResult {
+		private Class<?> clazz;
+		private ClassNotFoundException ex;
+		protected static LoadClassResult NOT_FOUND = new LoadClassResult();
 
-    protected static class LoadClassResult {
-        private Class<?> clazz;
-        private ClassNotFoundException ex;
+		public LoadClassResult() {
+		}
 
-        public LoadClassResult() {
-        }
+		public LoadClassResult(ClassNotFoundException ex) {
+			this.ex = ex;
+		}
 
-        public LoadClassResult(ClassNotFoundException ex) {
-            this.ex = ex;
-        }
+		public Class<?> getClazz() {
+			return clazz;
+		}
 
-        public Class<?> getClazz() {
-            return clazz;
-        }
+		public void setClazz(Class<?> clazz) {
+			this.clazz = clazz;
+		}
 
-        public void setClazz(Class<?> clazz) {
-            this.clazz = clazz;
-        }
+		public ClassNotFoundException getEx() {
+			return ex;
+		}
 
-        public ClassNotFoundException getEx() {
-            return ex;
-        }
+		public void setEx(ClassNotFoundException ex) {
+			this.ex = ex;
+		}
+	}
 
-        public void setEx(ClassNotFoundException ex) {
-            this.ex = ex;
-        }
-    }
+	private Class<?> loadClassWithCache(String name, boolean resolve) throws ClassNotFoundException {
+		LoadClassResult result = classCache.get(name);
+		if (result != null) {
+			if (result.getEx() != null) {
+				throw result.getEx();
+			}
+			return result.getClazz();
+		}
 
-    private Class<?> loadClassWithCache(String name, boolean resolve) throws ClassNotFoundException {
-
-        LoadClassResult resultInCache = classCache.get(name, it -> {
-            LoadClassResult r = new LoadClassResult();
-            try {
-                Class<?> clazz = CachedLaunchedURLClassLoader.super.loadClass(it, resolve);
-                r.setClazz(clazz);
-            } catch (ClassNotFoundException e) {
-                r.setEx(e);
-            }
-            return r;
-        });
-
-        if (resultInCache.getEx() != null) {
-            throw resultInCache.getEx();
-        }
-        return resultInCache.getClazz();
-    }
+		try {
+			Class<?> clazz = super.findLoadedClass(name);
+			if (clazz == null) {
+				clazz = super.loadClass(name, resolve);
+			}
+			if (clazz == null) {
+				classCache.put(name, LoadClassResult.NOT_FOUND);
+			}
+			return clazz;
+		} catch (ClassNotFoundException exception) {
+			classCache.put(name, new LoadClassResult(exception));
+			throw exception;
+		}
+	}
 }
